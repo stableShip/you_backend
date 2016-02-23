ab7221694728fe391202762ce03d0284db0be4fd
stableShip <296888680@qq.com>
2016-02-23 16:38:44 +0800

    删除前台显示页面

--- src/main/java/com/easygame/sdk/service/impl/backend/ad/GamecenterAdBizImpl.java
+++ src/main/java/com/easygame/sdk/service/impl/backend/ad/GamecenterAdBizImpl.java
@@ -3,7 +3,6 @@ package com.easygame.sdk.service.impl.backend.ad;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
